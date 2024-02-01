4cfacfa8f2ec6fc116294118f54d5d48bfa10a67
Juewon Seo <j1sodamdev@gmail.com>
2024-02-02 00:55:26 +0900

    commit

--- web_work/final_project/final_project/settings.py
+++ web_work/final_project/final_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'main_app',
     'deep_app',
     'accounts',
