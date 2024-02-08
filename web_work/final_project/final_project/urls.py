6e80bca6378ef5d92089aa09b6568ecf7265bed8
tting99 <kmjkth1@naver.com>
2024-02-08 16:49:04 +0900

    최종
    .

--- web_work/final_project/final_project/urls.py
+++ web_work/final_project/final_project/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')), # 메인 시작화면 주소
-    path('', include('deep_app.urls')),
+    path('deep/', include('deep_app.urls')),
     path('accounts/', include('accounts.urls')),
-    # path('input_image/result', include('deep_app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
